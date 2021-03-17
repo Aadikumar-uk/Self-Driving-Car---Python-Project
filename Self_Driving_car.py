349cba9f2db06bd35a88444996b415178a58dafa
Aadi Kumar <67184378+Aadikumar-uk@users.noreply.github.com>
2021-03-17 21:36:39 +0530

    Corrected one word

--- Self_Driving_car.py
+++ Self_Driving_car.py
@@ -4,4 +4,4 @@ window = pygame.display.set_mode((1200, 400))
 track = pygame.image.load('track1.png')
 while True:
     window.blit(track, (0, 0))
-    pygame.disply.update()
+    pygame.display.update()
