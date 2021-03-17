43f76c421aa4c8053104db96784d73b078bc0283
Aadi Kumar <67184378+Aadikumar-uk@users.noreply.github.com>
2021-03-17 21:47:06 +0530

    Updated

--- Self_Driving_car.py
+++ Self_Driving_car.py
@@ -2,6 +2,7 @@ import pygame
 pygame.init()
 window = pygame.display.set_mode((1200, 400))
 track = pygame.image.load('track1.png')
+car = pygame.image.load('tesla.png')
 while True:
     window.blit(track, (0, 0))
     pygame.display.update()
